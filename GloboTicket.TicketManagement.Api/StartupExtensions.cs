0857eff9106699a72e6bf095581eddd1d57ab36d
Eamon Keane <e.keane10@gmail.com>
2022-12-20 08:38:03 +0000

    swagger and Export file added

--- GloboTicket.TicketManagement.Api/StartupExtensions.cs
+++ GloboTicket.TicketManagement.Api/StartupExtensions.cs
@@ -1,7 +1,9 @@
-﻿using GloboTicket.TicketManagement.Application;
+﻿using GloboTicket.TicketManagement.Api.Utility;
+using GloboTicket.TicketManagement.Application;
 using GloboTicket.TicketManagement.Infrastructure;
 using GloboTicket.TicketManagement.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 namespace GloboTicket.TicketManagement.Api
 {
@@ -10,7 +12,8 @@ namespace GloboTicket.TicketManagement.Api
          public static WebApplication ConfigureServices(
         this WebApplicationBuilder builder)
         {
-          
+            
+            AddSwagger(builder.Services);
 
             builder.Services.AddApplicationServices();
             builder.Services.AddInfrastructureServices(builder.Configuration);
@@ -30,6 +33,15 @@ namespace GloboTicket.TicketManagement.Api
         public static WebApplication ConfigurePipeline(this WebApplication app)
         {
 
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "GloboTicket Ticket Management API");
+                });
+            }
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
@@ -44,6 +56,22 @@ namespace GloboTicket.TicketManagement.Api
 
             return app;
 
+        }
+        private static void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+
+
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "GloboTicket Ticket Management API",
+
+                });
+
+                c.OperationFilter<FileResultContentTypeOperationFilter>();
+            });
         }
 
         public static async Task ResetDatabaseAsync(this WebApplication app)
--- GloboTicket.TicketManagement.Application/Profiles/MappingProfile.cs
+++ GloboTicket.TicketManagement.Application/Profiles/MappingProfile.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Features.Categories.Commands.CreateCateogry;
 using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
 using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesListWithEvents;
 using GloboTicket.TicketManagement.Application.Features.Events.Commands.CreateEvent;
+using GloboTicket.TicketManagement.Application.Features.Events.Commands.UpdateEvent;
 using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventDetail;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsExport;
 using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsList;
 using GloboTicket.TicketManagement.Domain.Entities;
 using System;
@@ -18,13 +21,18 @@ namespace GloboTicket.TicketManagement.Application.Profiles
         public MappingProfile()
         {
             CreateMap<Event, EventListVm>().ReverseMap();
+            CreateMap<Event, CreateEventCommand>().ReverseMap();
+            CreateMap<Event, UpdateEventCommand>().ReverseMap();
             CreateMap<Event, EventDetailVm>().ReverseMap();
+            CreateMap<Event, CategoryEventDto>().ReverseMap();
+            CreateMap<Event, EventExportDto>().ReverseMap();
 
             CreateMap<Category, CategoryDto>();
             CreateMap<Category, CategoryListVm>();
             CreateMap<Category, CategoryEventListVm>();
+            CreateMap<Category, CreateCategoryCommand>();
+            CreateMap<Category, CreateCategoryDto>();
 
-            CreateMap<Event, CreateEventCommand>();
 
         }
     }
